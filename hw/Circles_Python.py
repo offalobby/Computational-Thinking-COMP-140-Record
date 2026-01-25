83e8bc7df3d7c1749340d4a521cd115e8a2bff16
offalobby <186242941+offalobby@users.noreply.github.com>
2026-01-25 00:04:54 -0600

    Add Circles Function Template

---
+++ hw/Circles_Python.py
@@ -0,0 +1,110 @@
+"""
+Code to calculate the circle that passes through three given points.
+
+Fill in each function with your code (including fixing the return
+statement).
+"""
+
+import math
+import comp140_module1 as circles
+
+def distance(point0x, point0y, point1x, point1y):
+    """
+    Computes the distance between two points.
+
+    inputs:
+        -point0x: a float representing the x-coordinate of the first point
+        -point0y: a float representing the y-coordinate of the first point
+        -point1x: a float representing the x-coordinate of the second point
+        -point1y: a float representing the y-coordinate of the second point
+
+    returns: a float that is the distance between the two points
+    """
+    
+    return 0.0
+
+def midpoint(point0x, point0y, point1x, point1y):
+    """
+    Computes the midpoint between two points.
+
+    inputs:
+        -point0x: a float representing the x-coordinate of the first point
+        -point0y: a float representing the y-coordinate of the first point
+        -point1x: a float representing the x-coordinate of the second point
+        -point1y: a float representing the y-coordinate of the second point
+
+    returns: two floats that are the x- and y-coordinates of the midpoint
+    """
+    return 0.0, 0.0
+
+def slope(point0x, point0y, point1x, point1y):
+    """
+    Computes the slope of the line that connects two given points.
+
+    The x-values of the two points, point0x and poin1x, must be different.
+
+    inputs:
+        -point0x: a float representing the x-coordinate of the first point.
+        -point0y: a float representing the y-coordinate of the first point
+        -point1x: a float representing the x-coordinate of the second point.
+        -point1y: a float representing the y-coordinate of the second point
+
+    returns: a float that is the slope between the points
+    """
+    return 0.0
+
+def perp(lineslope):
+    """
+    Computes the slope of a line perpendicular to a given slope.
+
+    input:
+        -lineslope: a float representing the slope of a line.
+                    Must be non-zero
+
+    returns: a float that is the perpendicular slope
+    """
+    return 0.0
+
+def intersect(slope0, point0x, point0y, slope1, point1x, point1y):
+    """
+    Computes the intersection point of two lines.
+
+    The two slopes, slope0 and slope1, must be different.
+
+    inputs:
+        -slope0: a float representing the slope of the first line.
+        -point0x: a float representing the x-coordinate of the first point
+        -point0y: a float representing the y-coordinate of the first point
+        -slope1: a float representing the slope of the second line.
+        -point1x: a float representing the x-coordinate of the second point
+        -point1y: a float representing the y-coordinate of the second point
+
+    returns: two floats that are the x- and y-coordinates of the intersection
+    point
+    """
+    return 0.0, 0.0
+
+def make_circle(point0x, point0y, point1x, point1y, point2x, point2y):
+    """
+    Computes the center and radius of a circle that passes through
+    thre given points.
+
+    The points must not be co-linear and no two points can have the
+    same x or y values.
+
+    inputs:
+        -point0x: a float representing the x-coordinate of the first point
+        -point0y: a float representing the y-coordinate of the first point
+        -point1x: a float representing the x-coordinate of the second point
+        -point1y: a float representing the y-coordinate of the second point
+        -point2x: a float representing the x-coordinate of the third point
+        -point2y: a float representing the y-coordinate of the third point
+
+    returns: three floats that are the x- and y-coordinates of the center
+    and the radius
+    """
+    return 0.0, 0.0, 0.0
+
+# Run GUI - uncomment the line below after you have
+#           implemented make_circle
+#circles.start(make_circle)
